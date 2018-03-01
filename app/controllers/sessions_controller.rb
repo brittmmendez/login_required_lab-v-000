5d154ceb0b7549df9bf877cb37244a05885ef2e8
Brittany Morris <brittmmendez@gmail.com>
2018-03-01 13:23:10 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,10 @@
 class SessionsController<ApplicationController
 def create
-
+  if !params[:name] || params[:name].empty?
+    redirect_to controller: 'sessions', action: 'new'
+  else
+    session[:name]=params[:name]
+  end
 end
 
 def destroy
